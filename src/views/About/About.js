016ab314140841e1028230ed5dfaa64bcf51323e
xxj95719 <87416959@qq.com>
2019-01-03 14:35:15 +0800

    😄

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import style from './About.scss'
-
+import React from "react";
+import style from "./About.scss";
+import qrcode from "../../assets/qrcode.jpg";
 class About extends React.Component {
-  state = {}
+  state = {};
   componentDidMount() {}
   componentWillUnmount() {}
   render() {
-    return <div />
+    return (
+      <div className={style.about}>
+        <img src={qrcode} alt="如同鸡讲" />
+        <p className={style.tips}>快来骚我啊~</p>
+      </div>
+    );
   }
 }
 
-export default About
+export default About;
